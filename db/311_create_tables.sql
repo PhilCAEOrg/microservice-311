92a6aace63de17a24fe2f806f6deeedb9cf337fe
PhilCAEUser <mail@rwth>
2020-10-14 14:52:22 +0000

    Test

--- db/311_create_tables.sql
+++ db/311_create_tables.sql
@@ -3,6 +3,14 @@
 -- Automatically generated sql script for the service 311, created by the CAE.
 -- --------------------------------------------------------
 
+--
+-- Table structure for table tab.
+--
+CREATE TABLE caeschema.tab (
+  ce VARCHAR(255) ,
+CONSTRAINT ce_PK PRIMARY KEY (ce)
+ 
+);
 
 
 
